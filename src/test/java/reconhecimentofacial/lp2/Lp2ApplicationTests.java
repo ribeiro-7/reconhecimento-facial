7a42f4f641a2d541a16b1df3cea3e4045b7bb4f8
ribeirin-7 <arthurr366@gmail.com>
2025-01-22 14:49:02 -0300

    Projeto Reconhecimento facial

---
+++ src/test/java/reconhecimentofacial/lp2/Lp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package reconhecimentofacial.lp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
